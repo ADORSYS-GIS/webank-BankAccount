58993962bf68a5c36a5da798abe4de097238e6c6
NkwaTambe <nkwajudetambe@gmail.com>
2024-11-01 08:54:06 +0100

    fix: add missing name attribute to Feign client configuration

--- ledgers-bank-account/ledgers-bank-account-rest-client/src/main/java/de/adorsys/ledgers/bank/client/rest/AccountMgmtRestClient.java
+++ ledgers-bank-account/ledgers-bank-account-rest-client/src/main/java/de/adorsys/ledgers/bank/client/rest/AccountMgmtRestClient.java
@@ -2,6 +2,6 @@ package de.adorsys.ledgers.bank.client.rest;
 import  de.adorsys.ledgers.bank.api.resource.AccountMgmResourceAPI;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(path = AccountMgmResourceAPI.BASE_PATH)
+@FeignClient(name = "account-management-service", path = AccountMgmResourceAPI.BASE_PATH)
     public interface AccountMgmtRestClient extends AccountMgmResourceAPI {}
 
