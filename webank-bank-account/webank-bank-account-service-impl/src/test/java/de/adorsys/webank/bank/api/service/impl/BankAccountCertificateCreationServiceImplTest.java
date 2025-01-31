5f67ad30689bbcbe28f4392c1fd5d64bf06b100d
Menkene Koufan <Menkene.Koufan@adorsys.com>
2025-01-31 12:13:04 +0100

    refactored test to increase coverage

--- webank-bank-account/webank-bank-account-service-impl/src/test/java/de/adorsys/webank/bank/api/service/impl/BankAccountCertificateCreationServiceImplTest.java
+++ webank-bank-account/webank-bank-account-service-impl/src/test/java/de/adorsys/webank/bank/api/service/impl/BankAccountCertificateCreationServiceImplTest.java
@@ -52,12 +52,15 @@ class BankAccountCertificateCreationServiceImplTest {
     void testRegisterNewBankAccount_Failure() {
         when(bankAccountService.createNewAccount(any(), any(), any())).thenReturn(null);
 
-        assertThrows(IllegalStateException.class, () -> {  // Fixed typo in exception name
-            BankAccountBO account = new BankAccountBO();  // Separate object creation
-            certificateCreationService.registerNewBankAccount(  // Single throwing invocation
-                    "123", "key", account, "user", "branch"  // Use correct BankAccountBO class
-            );
-        });
+        // Create object OUTSIDE the lambda
+        BankAccountBO account = new BankAccountBO(); // Fix class name typo (B0 -> BO)
+
+        assertThrows(IllegalStateException.class, () ->
+                // Only the service call remains in the lambda
+                certificateCreationService.registerNewBankAccount(
+                        "123", "key", account, "user", "branch"
+                )
+        );
     }
 
     @Test
